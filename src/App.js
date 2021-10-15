d45ed0b959c9370a276736c438d50f6f5d2faabe
David Baba <davidbabasalifu2020@gmail.com>
2021-10-15 03:36:18 +0100

    Completed the routing, about page and db connection and CRUD operationns using json-server inbeded db

--- src/App.js
+++ src/App.js
@@ -1,64 +1,101 @@
-import { useState } from 'react';
-import { FaLess, FaLessThanEqual } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AddTask from './components/AddTask';
 import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
 import Task from './components/Tasks';
 
 
 function App() {
   const [showAddTaskForm, setShowAddTaskForm] = useState(false)
-  const [tasks, setTasks] = useState([
-      {
-          id: 1,
-          text:'GSU Gettogether',
-          day: 'Oct 10Th at 2:30pm',
-          reminder: true,   
-      },
-      {
-          id: 2,
-          text:'Devotional Test',
-          day: 'Oct 13Th at 3:0am',
-          reminder: true,   
-      },
-      {
-          id: 3,
-          text:'Raect Classes',
-          day: 'Oct 20Th at 8:30am',
-          reminder: false,   
-      },
-      {
-          id: 4,
-          text:'DB Classes',
-          day: 'Oct 30Th at 7:30am',
-          reminder: true,   
-      }
-  ])
+  const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    const getTask = async ()=>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTask()
+  }, [])
+
+  //Fetch Tasks from server
+  const fetchTasks = async () =>{
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+  
+  //Fetch Task from server (Only One)
+  const fetchTask = async (id) =>{
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+  
   //Add Task
-  const addTask = (task) =>{
-  const id = Math.floor(Math.random()*10000) + 1
-  const newTAsk = {id, ...task}
-  setTasks([...tasks,newTAsk])
+  const addTask = async (task) =>{
+    const res = await fetch(`http://localhost:5000/tasks/`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(task) 
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
+
+
+  // const id = Math.floor(Math.random()*10000) + 1
+  // const newTAsk = {id, ...task}
+  // setTasks([...tasks,newTAsk])
   }
 
   //For deleting task
-  const deleteTask = (id) =>{
+  const deleteTask = async (id) =>{
+    await fetch(`http://localhost:5000/tasks/${id}`, 
+    {
+      method: 'DELETE',
+    })
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
-  //To set the reminder 
-  const toggleReminder = (id) => {
+  //Toggle to set the reminder 
+  const toggleReminder = async (id) => {
+    const taskToggle = await fetchTask(id)
+    const updatedTask = {...taskToggle, reminder: !taskToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
     setTasks(tasks.map((task) => 
-    task.id === id? {...task, reminder: !task.reminder}: task
+    task.id === id? {...task, reminder: data.reminder}: task
     ))
   }
 
   return (
-    <div className="container">
-      <Header onAdd = {() => setShowAddTaskForm(!showAddTaskForm)} showAdd = {showAddTaskForm}/>
-      {showAddTaskForm && <AddTask onAdd ={addTask} />}
-      {tasks.length > 0 ? (<Task tasks = {tasks} onDelete ={deleteTask} onToggle = {toggleReminder} /> ) : ('No Task to show')}
-    </div>
+    <Router>
+        <div className="container">
+          <Header onAdd = {() => setShowAddTaskForm(!showAddTaskForm)} showAdd = {showAddTaskForm}/>
+          
+          <Route path ='/' exact render= {(props)=>(
+            <>
+              {showAddTaskForm && <AddTask onAdd ={addTask} />}
+          {tasks.length > 0 ? (<Task tasks = {tasks} onDelete ={deleteTask} onToggle = {toggleReminder} /> ) : ('No Task to show')}
+            </>
+          )}/>
+          <Route path ='/about' component = {About}/>
+          <Footer />
+        </div>
+    </Router>
   );
 }
 
